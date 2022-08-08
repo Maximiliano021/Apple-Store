8284eb62dbce51316c490730e59ef470b647d142
Maximiliano021 <escuderomaximiliano99@gmail.com>
2022-08-07 23:07:50 -0300

    agregando modo oscuro y modificaciones de estilo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let btnBuy, carritoCanvas, spanCantidadProd, btnCarrito, mainHTML, allProductos, modalBody, modalFooter;
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+let modoOscuro = localStorage.getItem('modoOscuro') == 'true'? true : false;
 
 const productos = [];
 
@@ -29,6 +30,7 @@ const initiatePage = () => {
     crearHTML();
     carritoUI();
     mostrarProductos();
+    cambiarEstilo(modoOscuro);
     document.addEventListener('click', eventosClick);
 }
 
@@ -50,7 +52,7 @@ let crearHTML = () => {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">COMPRA</h5>
-                            <button type="button" class="btn-close close-modal" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <a type="button" class="close-modal" data-bs-dismiss="modal" aria-label="Close"></a>
                         </div>
                         <div class="modal-body">
                         </div>
@@ -60,11 +62,11 @@ let crearHTML = () => {
                 </div>
             </div>
             <div class="main" data-aos="fade-zoom-in" data-aos-delay="100">
-                <div class="categoria row" id="NOTEBOOK"></div>
+                <div class="categoria row" id="MACBOOK"></div>
                 <div class="categoria row" id="IPAD"></div>
-                <div class="categoria row" id="CELULAR"></div>
-                <div class="categoria row" id="RELOJ"></div>
-                <div class="categoria row" id="AURICULAR"></div>
+                <div class="categoria row" id="IPHONE"></div>
+                <div class="categoria row" id="WATCH"></div>
+                <div class="categoria row" id="AIRPOD"></div>
                 <div class="categoria all-productos"></div>
             </div>
         </div>`;
@@ -85,7 +87,7 @@ function mostrarProductos() {
                 <div class="contain-img p-3 bg-white">
                     <img src="${producto.srcImagen}" class="card-img-top prod-img" alt="${producto.nombre}">
                 </div>
-                <div class="card-body ">
+                <div class="card-body">
                     <h5 class="card-title fw-bold text-center">${producto.nombre}</h5>
                     <p class="card-text fw-light">and make up the bulk of the card's content.</p>
                     <div class="d-flex justify-content-between align-items-center">
@@ -94,8 +96,9 @@ function mostrarProductos() {
                     </div>
                 </div>
             </div>`;
-        delay == 150 ? delay = 50 : delay += 50;
-        contentCagoria.innerHTML = `<h3 class="fw-bolder">LO MEJOR DE LA CATEGORIA ${producto.categoria}</h3><p class="w-50">${lorem()}</p>`;
+        delay = transicionDelay(delay);
+        contentCagoria.innerHTML = `
+            <h3 class="fw-bolder">LO MEJOR DE LA CATEGORIA ${producto.categoria}</h3><p class="w-50">${lorem()}</p>`;
 
         if (categoria.textContent == '')
             categoria.append(contentCagoria);
@@ -105,22 +108,35 @@ function mostrarProductos() {
     })
 }
 
+
 function eventosClick(e) {
     const targetSelect = e.target;
     let productoDevuelto = productos.find(e => e.id == targetSelect.id);
 
     if (targetSelect.classList.contains('buy-prod')) {
         actualizarCarrito(productoDevuelto)
-        detalleProducto(productoDevuelto);
+        detalleEnModal(productoDevuelto);
     }
 
+    if (targetSelect.classList.contains('btn-mod')){
+        if(modoOscuro){
+            modoOscuro = false;
+            mostrarAlert('Modo Light');
+        }
+        else{
+            modoOscuro = true;
+            mostrarAlert('Modo Ocuro');
+        } 
+        localStorage.setItem('modoOscuro', modoOscuro);
+        cambiarEstilo(modoOscuro);
+    }
     if (targetSelect.classList.contains('agregar')) {
         abrirCanvas();
-        detalleProducto(productoDevuelto);
+        detalleEnModal(productoDevuelto);
     }
 
     if (targetSelect.classList.contains('delete-prod'))
-        eliminarCarrito(targetSelect);
+        eliminarDeCarrito(targetSelect);
 
     if (targetSelect.classList.contains('no-buy'))
         btnCloseCanvas.click();
@@ -141,22 +157,25 @@ function actualizarCarrito(nuevoProducto) {
         prodEncontrado.precioTotal += prodEncontrado.precioTotal;
     }
     mostrarAlert('Agregado');
-    sincronizarLocalStorage();
+    actualizarLocalStorage();
     carritoUI();
 }
 
 function carritoUI() {
     let totalSuma = 0;
     let totalCantidad = 0;
-    let detalleProducto;
+    let detalle;
     carritoCanvas.innerHTML = '';
     carrito.forEach(prod => {
-        detalleProducto = document.createElement('div')
-        detalleProducto.innerHTML = `
+        detalle = document.createElement('div')
+        detalle.innerHTML = `
             <div class="d-flex justify-content-between">
-                <img src="${prod.producto.srcImagen}" class="img-prod-carrito" alt="...">
+                <div class="container-img">
+                    <img src="${prod.producto.srcImagen}" class="img-prod-carrito" alt="...">
+                </div>
+                
                 <h5 class="card-title fw-bold text-center">${prod.producto.nombre}</h5>
-                <button type="button" class="btn-close delete-prod" id="${prod.producto.id}" aria-label="Close"></button>
+                <i class='bx bxs-trash-alt bx-sm delete-prod' id="${prod.producto.id}"></i>
                 </div>
                 <div class="d-block m-2">
                 <div class="d-flex justify-content-between">
@@ -164,24 +183,24 @@ function carritoUI() {
                     <label>Cantidad</label>
                     <input style="width:50px" class="mx-2 inputCantidad" id="${prod.producto.id}" type="number" min="1" value="${prod.cantidad}">
                     </div>
-                    <span class="fw-bold precio fs-5">$${prod.precioTotal}USD</span>
+                    <span class="fw-bold precio fs-5">$${prod.precioTotal}  USD</span>
                 </div>
             </div>`;
         totalCantidad += prod.cantidad;
-        detalleProducto.classList.add('producto-carrito');
-        carritoCanvas.append(detalleProducto);
+        detalle.classList.add('producto-carrito');
+        carritoCanvas.append(detalle);
     })
     carrito.map(prod => totalSuma += prod.producto.precio * prod.cantidad)
-    document.getElementById('totalCarrito').textContent = `$${totalSuma.toFixed(3)}`;
+    document.getElementById('totalCarrito').textContent = `$${totalSuma.toFixed(2)}`;
 
     //actualiza cantidad de productos en el html 
     totalCantidad == 0 ? spanCantidadProd.textContent = ''
         : spanCantidadProd.textContent = totalCantidad;
 }
 
-function detalleProducto(selectProducto) {
-    let detalle = document.createElement('div');
+function detalleEnModal(selectProducto) {
     document.querySelector('.close-modal').click();
+    let detalle = document.createElement('div');
 
     modalFooter.textContent = '';
     modalBody.textContent = '';
@@ -196,10 +215,10 @@ function detalleProducto(selectProducto) {
             <div class="col-11 col-lg-6 col-mb-10 justify-content-start p-1 p-lg-0 my-3 my-lg-0">
             <div class="description col-12 col-lg-12">
                 <h2 class="description-title mb-4">${selectProducto.nombre}</h2>
-                <p class="text-black-50 mb-3">${lorem()}</p>
+                <p class="descripcion-producto mb-3">${lorem()}</p>
                 <div class="col-12 col-lg-6 d-lg-block d-flex justify-content-lg-center justify-content-between  align-items-center">
                 <div class="col-5 my-1 d-flex justify-content-lg-start justify-content-between align-items-center">
-                    <span class="fs-3 text-black precio fw-bold">${selectProducto.precio}</span>
+                    <span class="fs-3 precio fw-bold">${selectProducto.precio}</span>
                     <label class="text-center descuento mx-3 pb-1 pe-2 ps-2">50%</label>
                 </div>
                 <strike class="opacity-25 fw-bold">$250.00</strike>
@@ -217,12 +236,13 @@ function detalleProducto(selectProducto) {
 
     detalle.classList.add('container');
     modalBody.append(detalle);
+    estiloModal();
 }
 
-function eliminarCarrito(elemento) {
-    let prodCarrito = carrito.find(prod => prod.producto.id == elemento.id)
-    carrito = carrito.filter(prod => prod.producto.id != prodCarrito.producto.id)
-    sincronizarLocalStorage();
+function eliminarDeCarrito(elemento) {
+    let prodCarrito = carrito.find(prod => prod.producto.id == elemento.id);
+    carrito = carrito.filter(prod => prod.producto.id != prodCarrito.producto.id);
+    actualizarLocalStorage();
     carritoUI();
 }
 
@@ -242,14 +262,14 @@ function asignarClassId() {
 }
 
 
-function sincronizarLocalStorage() {
+function actualizarLocalStorage() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
 function mostrarAlert(mensaje) {
     Toastify({
         text: mensaje,
-        duration: 1000,
+        duration: 1500,
         position: "center"
     }).showToast();
 }
@@ -258,10 +278,59 @@ function actualizarInput(targetSelect) {
     const elegido = carrito.find(e => e.producto.id == targetSelect.id);
     if (targetSelect.value != '') {
         elegido.cantidad = Number(targetSelect.value)
-        elegido.precioTotal = Number((elegido.cantidad * elegido.producto.precio).toFixed(3));
+        elegido.precioTotal = Number((elegido.cantidad * elegido.producto.precio).toFixed(2));
         carritoUI();
     }
-    sincronizarLocalStorage();
+    actualizarLocalStorage();
 }
 
 const abrirCanvas = () => btnCarrito.click();
+
+const transicionDelay = (delay)=>{
+    return delay == 150 ? delay = 50 : delay += 50;
+}
+
+function cambiarEstilo(modoOscuro){
+    let catalogo = document.querySelector('.content');
+    let carritoBody = document.querySelector('.offcanvas-body');
+    let carritoFooter = document.querySelector('.offcanvas-footer');
+    let carritoFooterText = document.querySelector('.text-canvas-footer');
+    let listaCardBody = document.querySelectorAll('.card-body');
+    let footerHeader = document.querySelector('.footer-header');
+    let footerMain = document.querySelector('.footer-main');
+    
+    if(modoOscuro){
+        catalogo.classList.add('bg-dark', 'text-light');
+        carritoBody.classList.add('bg-dark','box-light','text-light')
+        carritoFooter.classList.add('bg-secondary');
+        carritoFooterText.classList.add('text-light');
+        listaCardBody.forEach(card=>card.classList.add('box-light'));
+        footerHeader.classList.add('bg-secondary', 'bg-opacity-10')
+        footerMain.classList.add('bg-secondary', 'bg-opacity-50');
+    }
+    else{
+        catalogo.classList.remove('bg-dark', 'text-light');
+        carritoBody.classList.remove('bg-dark','box-light','text-light')
+        carritoFooter.classList.remove('bg-secondary');
+        carritoFooterText.classList.remove('text-light');
+        listaCardBody.forEach(card=>card.classList.remove('box-light'));
+        footerHeader.classList.remove('bg-secondary','bg-opacity-10')
+        footerMain.classList.remove('bg-secondary','bg-opacity-50');
+    }
+}
+
+function estiloModal(){
+    let modalContent = document.querySelector('.modal-content')
+    let modalDescripcion = document.querySelector('.descripcion-producto');
+
+    if(modoOscuro){
+        modalContent.classList.add('bg-dark');
+        modalContent.classList.add('box-light');
+        modalDescripcion.classList.add('text-light');
+    }
+    else{
+        modalContent.classList.remove('bg-dark');
+        modalContent.classList.remove('box-light');
+        modalDescripcion.classList.remove('text-light');
+    }
+}
